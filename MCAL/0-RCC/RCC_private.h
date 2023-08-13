8e3e2d85a69fbeed4ec5093903ad9264afbd9533
Abderrahmane Lahouasna <103077345+Mesteriwan82@users.noreply.github.com>
2023-08-13 13:39:50 +0100

    Update RCC_private.h

--- MCAL/0-RCC/RCC_private.h
+++ MCAL/0-RCC/RCC_private.h
@@ -33,7 +33,22 @@
 #define RCC_BDCR                    *((volatile u32*)(RCC_base_adress + RCC_BDCR_ofsset))
 #define RCC_CSR_ofsset              (0x24U)
 #define RCC_CSR                     *((volatile u32*)(RCC_base_adress + RCC_CSR_ofsset))
+typedef struct 
+{
+volatile u32 CR       ;  //RCC_base_adress + RCC_CR_ofsset        (0x00U)
+volatile u32 CFGR     ;  //RCC_base_adress + RCC_CFGR_ofsset      (0x04U)
+volatile u32 CIR      ;  //RCC_base_adress + RCC_CIR_ofsset       (0x08U)
+volatile u32 APB2RSTR ;  //RCC_base_adress + RCC_APB2RSTR_ofsset  (0x0CU)
+volatile u32 APB1RSTR ;  //RCC_base_adress + RCC_APB1RSTR_ofsset  (0x10U)
+volatile u32 AHBENR   ;  //RCC_base_adress + RCC_AHBENR_ofsset    (0x14U)
+volatile u32 APB2ENR  ;  //RCC_base_adress + RCC_APB2ENR_ofsset   (0x18U)
+volatile u32 APB1ENR  ;  //RCC_base_adress + RCC_APB1ENR_ofsset   (0x1CU)
+volatile u32 BDCR     ;  //RCC_base_adress + RCC_BDCR_ofsset      (0x20U)
+volatile u32 CSR      ;  //RCC_base_adress + RCC_CSR_ofsset       (0x24U)	
+}RCC_TDef;
 
+#define MRCC     ((RCC_TDef*)RCC_base_adress)
+ 
 
 
 
